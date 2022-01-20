853b3d1d6546367123f238d7128c45f59d3d1543
V-B-Bozhinskiy <bove203@outlook.com>
2022-01-20 20:21:30 +0300

    parser

--- routes/console.php
+++ routes/console.php
@@ -14,6 +14,64 @@ use Illuminate\Support\Facades\Artisan;
 | simple approach to interacting with each command's IO methods.
 |
 */
+Artisan::command('parceEkatalog', function () {
+    $i = 0;
+    do {
+        $url = 'https://www.e-katalog.ru/ek-list.php?katalog_=189&search_=rtx+3090'."&page_=$i";
+        //$url = 'https://yandex.ru';
+
+        $data = file_get_contents($url);
+
+        //dd($data);
+
+        $dom = new DomDocument();
+        @$dom->loadHTML($data); // @-это оператор игнорирования ошибки при операции
+
+        $xpath = new DOMXPath($dom);
+        $divs = $xpath->query("//div[@class='model-short-div list-item--goods   ']");
+        
+        if ($i == 0){
+            $totalProductsString = $xpath->query("//span[@class='t-g-q']")[0]->nodeValue ?? false;
+
+            preg_match_all('/\d+/', $totalProductsString, $matches);
+            
+            $totalProducts = (int) $matches[0][0];
+
+            dump($totalProducts);
+            $productsOnOnePage = $divs->length;
+            $pages = ceil($totalProducts / $productsOnOnePage);
+            dump($pages);
+            $products = [];
+        }
+
+        foreach ($divs as $div){
+            $a = $xpath->query("descendant::a[@class='model-short-title no-u']", $div);
+            $name = $a[0]->nodeValue;
+
+            $price = 0;
+            $ranges = $xpath->query("descendant::div[@class='model-price-range']", $div);
+            if ($ranges->length == 1){
+                foreach ($ranges[0]->childNodes as $child){
+                    if ($child->nodeName == 'a'){
+                        $price = $child->nodeValue; 
+                    }
+                }
+            }
+            $ranges = $xpath->query("descendant::div[@class='pr31 ib']", $div);
+            if ($ranges->length == 1){
+                $price = $ranges[0]->nodeValue;
+            }
+            dump("$name: $price");
+            $products[] = [
+                'name' => $name,
+                'price' => $price
+            ];
+        }
+    $i++;
+    } while ($i<$pages);
+    dd($products);
+});
+
 Artisan::command('massInsert', function () {
     $categories = [
         [
