42362c37a5114f7175246445cde618cbafb5e1da
V-B-Bozhinskiy <bove203@outlook.com>
2022-03-19 18:54:55 +0300

    vue counter 3

---
+++ resources/js/components/NavBarCartComponent.vue
@@ -0,0 +1,27 @@
+<template>
+   <li class="nav-item">
+        <a class="nav-link" href="/cart">
+            Корзина <span id='cardProductsQuantity'>({{ cartProductsQuantity }})</span> 
+        </a>
+    </li>
+</template>
+
+<script>
+export default {
+    computed:{
+        cartProductsQuantity () {
+            return this.$store.state.cartProductsQuantity
+        }
+    },
+    mounted(){
+        axios.get('/cart/productsQuantity')
+            .then((response) => {
+                this.$store.dispatch('changeCartProductsQuantity', response.data)
+            })
+    }
+}
+</script>
+
+<style>
+
+</style>
